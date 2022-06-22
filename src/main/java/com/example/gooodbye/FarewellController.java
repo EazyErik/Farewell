f5f98fa4f0c935b09b3bd71e8660485f1a558f47
Erik Busch <erik_busch@gmx.de>
2022-06-22 11:28:16 +0200

    added farewellOptions

--- src/main/java/com/example/gooodbye/FarewellController.java
+++ src/main/java/com/example/gooodbye/FarewellController.java
@@ -6,12 +6,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Array;
+import java.util.List;
+
 @RestController
 @RequestMapping("api/farewell")
 public class FarewellController {
     @GetMapping("/{name}")
     public String goodBye(@PathVariable String name) {
-        return "Good bye " + name;
+       String[] farewellOptions = {"Adieu","Adios","See you later","au revoir"};
+       int index = (int)(Math.random() * 5) ;
+        return farewellOptions[index] + " " + name + "!";
     }
 
 }
